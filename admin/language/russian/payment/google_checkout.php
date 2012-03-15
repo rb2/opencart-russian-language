a4d719b08c52d46f5b2c6c879b17ecc31b0984c7
Ruslan Brest <rb@labtodo.com>
2012-03-15 23:31:12 +0200

    Google Checkout

--- admin/language/russian/payment/google_checkout.php
+++ admin/language/russian/payment/google_checkout.php
@@ -9,9 +9,13 @@ $_['text_success']       = 'Настройки модуля обновлены!'
 // Entry
 $_['entry_merchant_id']  = 'Merchant ID:';
 $_['entry_merchant_key'] = 'Merchant Key:';
-$_['entry_currency']     = 'Валюта:';
+$_['entry_callback']     = 'API callback URL:<br /><span class="help">Адрес страницы, на которой Google сможет уведомить вас о новых заказах и изменениях состояния заказа. Use Notification as HTML.</span>';
 $_['entry_test']         = 'Тестовый режим:';
+$_['entry_total']        = 'Минимальная сумма заказа:<br /><span class="help">Сумма заказа, после достижения которой данный способ станет доступен.</span>';
+$_['entry_order_status'] = 'Статус заказа:';
+$_['entry_geo_zone']	 = 'Географическая зона:';
 $_['entry_status']       = 'Статус:';
+$_['entry_sort_order']	 = 'Порядок сортировки:';
 
 // Error
 $_['error_permission']   = 'У Вас нет прав для управления этим модулем!';
--- catalog/language/russian/payment/google_checkout.php
+++ catalog/language/russian/payment/google_checkout.php
@@ -1,6 +1,4 @@
 <?php
 // Entry
-$_['entry_postcode'] = 'Индекс:';
-$_['entry_country']  = 'Страна:';
-$_['entry_zone']     = 'Регион / Область:';
+$_['text_title'] = 'Кредитная карта / Дебетовая карта (Google Checkout)';
 ?>
