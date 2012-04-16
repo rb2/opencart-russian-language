c03fa6149195f33093a8061bb3ef5ff4c02cd952
Ruslan Brest <rb@labtodo.com>
2012-04-16 05:12:38 +0300

    admin: Cancel = Вернуться.
    "Отмена" и "Возврат" вводят в cтупор на страницах просмотра заказа: отмена заказа? возврат товара?

--- bin/deployment.sh
+++ bin/deployment.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd ..
 git log --pretty=format:"%h %ci %s" > ./history.txt
-zip -r opencart-russian-lang-v1.5.1.3.zip admin catalog README.* history.txt
+zip -r opencart-russian-lang-v1.5.1.3.zip admin catalog README.* history.txt info.txt
+rm -f ./history.txt
