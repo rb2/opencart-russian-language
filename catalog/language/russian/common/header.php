1b21435a0db3e3c0b9c6d67ad7393a6bcbfc89fa
Ruslan Brest <rb@labtodo.com>
2012-05-20 22:43:30 +0300

    Корзина в шапке

--- catalog/language/russian/common/header.php
+++ catalog/language/russian/common/header.php
@@ -5,7 +5,7 @@ $_['text_blog']     = 'Блог';
 $_['text_wishlist'] = 'Закладки (%s)';
 $_['text_compare']  = 'Сравнение товаров (%s)';
 $_['text_cart']     = 'Корзина покупок';
-$_['text_items']    = '%s товар(а/ов) - %s';
+$_['text_items']    = 'Товаров: %s (%s)';
 $_['text_search']   = 'Поиск';
 $_['text_welcome']  = '<a href="%s">Войти</a> или <a href="%s">зарегистрироваться</a>.';
 $_['text_logged']   = 'Вы вошли как <a href="%s">%s</a> <b>(</b> <a href="%s">Выйти</a> <b>)</b>';
