3b9371bfd96dc034db26516f9c00222c30b70c10
Ruslan Brest <rb@labtodo.com>
2012-05-25 02:16:51 +0300

    admin: обновление перевода с SVN r1030 до тега v1521-release

--- admin/language/russian/payment/klarna.php
+++ admin/language/russian/payment/klarna.php
@@ -1,11 +1,11 @@
 <?php
 // Heading
-$_['heading_title']      = 'Klarna Invoice';
+$_['heading_title']      = 'Klarna';
 
 // Text
 $_['text_payment']        = 'Оплата';
 $_['text_success']        = 'Настройки модуля обновлены!';
-$_['text_klarna_invoice'] = '<a onclick="window.open(\'http://www.klarna.com\');"><img src="view/image/payment/klarna.png" alt="Klarna" title="Klarna" style="border: 1px solid #EEEEEE;" /></a>';
+// $_['text_klarna_invoice'] = '<a onclick="window.open(\'http://www.klarna.com\');"><img src="view/image/payment/klarna.png" alt="Klarna" title="Klarna" style="border: 1px solid #EEEEEE;" /></a>';
 $_['text_live']           = 'Live';
 $_['text_beta']           = 'Beta';
 
@@ -13,6 +13,10 @@ $_['text_beta']           = 'Beta';
 $_['entry_merchant']      = 'Klarna Merchant ID:<br /><span class="help">(estore id) для использования сервиса (предоставляется Klarna).</span>';
 $_['entry_secret']        = 'Klarna Secret:<br /><span class="help">Секретный ключ для использования сервиса (предоставляется Klarna).</span>';
 $_['entry_server']        = 'Сервер:';
+$_['entry_test']          = 'Тестовый режим:';
+$_['entry_invoice']       = 'Счёт-фактура:<br /><span class="help">Включить автоматическую активацию счетов-фактур?</span>';
+// @todo
+$_['entry_invoice_delay'] = 'Задержка счёта-фактуры:<br /><span class="help">When auto activating delay sending the invoice for x days.</span>';
 $_['entry_total']        = 'Минимальная сумма заказа:<br /><span class="help">Сумма заказа, после достижения которой данный способ станет доступен.</span>';
 $_['entry_order_status'] = 'Статус заказа:';
 $_['entry_status']       = 'Статус:';
@@ -20,7 +24,7 @@ $_['entry_sort_order']	 = 'Порядок сортировки:';
 
 // Error
 $_['error_permission']    = 'У Вас нет прав для управления этим модулем!';
-$_['error_xmlrpc']        = 'Для работы требуется PHP расширение XML-RPC!';
+// $_['error_xmlrpc']        = 'Для работы требуется PHP расширение XML-RPC!';
 $_['error_merchant']      = 'Отсутствует Klarna Merchant ID!';
 $_['error_secret']        = 'Отсутствует Klarna Secret!';
 ?>
