483a548ff13a9581d2d642613afb539aedaf98cc
Ruslan Brest <rb@labtodo.com>
2012-05-25 01:40:15 +0300

    catalog: обновление перевода с SVN r1030 до тега v1521-release

--- catalog/language/russian/payment/google_checkout.php
+++ catalog/language/russian/payment/google_checkout.php
@@ -1,4 +1,7 @@
 <?php
 // Entry
-$_['text_title'] = 'Кредитная карта / Дебетовая карта (Google Checkout)';
+// $_['text_title'] = 'Кредитная карта / Дебетовая карта (Google Checkout)';
+$_['entry_postcode'] = 'Индекс:';
+$_['entry_country']  = 'Страна:';
+$_['entry_zone']     = 'Область / геогр. регион:';
 ?>
--- catalog/language/russian/payment/klarna_pp.php
+++
@@ -1,4 +0,0 @@
-<?php
-// Text
-$_['text_title'] = 'Klarna Part Payment';
-?>
