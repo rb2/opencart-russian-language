bc4b736ef10d5eeeafc2cb3a995700b73247400a
Ruslan Brest <rb@labtodo.com>
2012-05-25 13:41:43 +0300

    MNT deployment script

--- bin/deployment.sh
+++ bin/deployment.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd ..
 git log --pretty=format:"%h %ci %s" > ./history.txt
-zip -r opencart-russian-lang-v1.5.2.x.zip admin catalog README.* history.txt
+zip -r opencart-russian-lang-v1.5.2.1.zip admin catalog README.* history.txt
 rm -f history.txt
