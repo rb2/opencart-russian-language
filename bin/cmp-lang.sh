dbbd7e16d78766fd281871599350054695407b5e
Ruslan Brest <rb@labtodo.com>
2013-03-26 22:21:57 +0200

    [~] MNT update tools

--- bin/cmp-lang.sh
+++ bin/cmp-lang.sh
@@ -2,10 +2,38 @@
 ## Compare Opencart language files
 ## Helps to compare EN and RU files *partially* (only variables, not counting translation string)
 
-## run script from ${CATALOG} folder: something like `cmp-lang voucher.php`
+## run script from ${CATALOG} folder
 ## the file voucher.php.diff will be created (overwritten)
 
-AREA='catalog'	## 'admin', 'catalog'
-CATALOG='total'
-## original files: projects/opencart-github-rb2.git
-diff -b <(cut -d'=' -f1 -s /home/rb/projects/opencart-github-rb2.git/upload/${AREA}/language/english/${CATALOG}/$1) <(cut -d'=' -f1 -s $1) > $1.diff
+function detect
+{
+	## original files are from projects/opencart-github-rb2.git
+	diff -b <(cut -d'=' -f1 -s /home/rb/projects/opencart-github-rb2.git/upload/${AREA}/language/english/${CATALOG}/$1) <(cut -d'=' -f1 -s $1) > $1.diff
+
+	# remove tmp file if its size = 0
+	[ -s $1.diff ] || rm $1.diff
+}
+
+PROJECT='/home/rb/projects/opencart-russian-language'
+
+AREA='admin'
+cd $PROJECT/${AREA}/language/russian/
+for CATALOG in catalog common design error extension feed localisation mail module payment report sale setting shipping tool total user
+do
+	cd ${CATALOG}
+	for file in *.php; do
+		detect $file
+	done
+	cd ..
+done
+
+AREA='catalog'
+cd $PROJECT/${AREA}/language/russian/
+for CATALOG in account affiliate checkout common error information mail module payment product shipping total
+do
+	cd ${CATALOG}
+	for file in *.php; do
+		detect $file
+	done
+	cd ..
+done
