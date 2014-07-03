3e20257827015743535d19ac5e72d0162a2d1e7a
Ruslan Brest <rb@labtodo.com>
2014-07-03 12:07:26 +0300

    return back more visible backlink to author's blog

--- catalog/language/russian/common/footer.php
+++ catalog/language/russian/common/footer.php
@@ -15,6 +15,5 @@ $_['text_login']        = 'Вход';
 $_['text_order']        = 'История заказов';
 $_['text_wishlist']     = 'Закладки';
 $_['text_newsletter']   = 'Рассылка';
-$_['text_powered']      = '%s <a style="text-decoration:none; color:gray;" href="http://rb.labtodo.com/page/opencart-1505-russian-language-pack" title="Русский перевод Опенкарт">&copy;</a> %s<br />
-Работает на <a href="http://www.opencart.com">OpenCart</a>';
+$_['text_powered']      = '%s &copy; %s<br />Работает на <a href="http://rb.labtodo.com/page/opencart-1505-russian-language-pack" title="Русский перевод Опенкарт">русском</a> <a href="http://www.opencart.com">OpenCart</a>';
 ?>
