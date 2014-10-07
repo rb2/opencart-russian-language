5e20da9e4b1b6a477fd255ad65d13b796add6106
Ruslan Brest <rb@labtodo.com>
2014-10-07 09:42:41 +0300

    [catalog] Ё-фикация слова введён

--- catalog/language/russian/api/customer.php
+++ catalog/language/russian/api/customer.php
@@ -3,9 +3,9 @@
 $_['text_success']       = 'You have successfully modified customers';
 
 // Error
-$_['error_permission']   = 'Warning: You do not have permission to access the API!';
+$_['error_permission']   = 'Вы не имеете достаточных прав для доступа к API!';
 $_['error_firstname']    = 'First Name must be between 1 and 32 characters!';
 $_['error_lastname']     = 'Last Name must be between 1 and 32 characters!';
-$_['error_email']        = 'E-Mail Address does not appear to be valid!';
+$_['error_email']        = 'E-Mail адрес введён неверно!';
 $_['error_telephone']    = 'Telephone must be between 3 and 32 characters!';
 $_['error_custom_field'] = '%s required!';
