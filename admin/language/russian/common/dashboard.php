f88ae7bde64ad279abd54b0b1c8750c72448a94e
Sergey Ogarkov <sogarkov@gmail.com>
2014-10-07 15:37:24 +0300

    [admin] admin/common translation

--- admin/language/russian/common/dashboard.php
+++ admin/language/russian/common/dashboard.php
@@ -3,46 +3,21 @@
 $_['heading_title']                = 'Обзор';
 
 // Text
-$_['text_welcome']                 = 'С возвращением, <strong>%s</strong>!';
-$_['text_new_order']               = 'Новые заказы';
-$_['text_new_customer']            = 'Новые покупатели';
-$_['text_total_sale']              = 'Всего продано';
-$_['text_analytics']               = 'Аналитика продаж';
-$_['text_online']                  = 'Люди онлайн';
-$_['text_activity']                = 'Последняя активность';
-$_['text_last_order']              = '5 последних заказов';
+$_['text_order_total']             = 'Всего заказов';
+$_['text_customer_total']          = 'Всего покупателей';
+$_['text_sale_total']              = 'Всего продаж';
+$_['text_online_total']            = 'Посетителей online';
+$_['text_map']                     = 'Карта мира';
+$_['text_sale']                    = 'Аналитика продаж';
+$_['text_activity']                = 'Последние события';
+$_['text_recent']                  = 'Последние заказы';
 $_['text_order']                   = 'Заказы';
 $_['text_customer']                = 'Покупатели';
-$_['text_day']                     = 'За день';
-$_['text_week']                    = 'За неделю';
-$_['text_month']                   = 'За месяц';
-$_['text_year']                    = 'За год';
-$_['text_customer_address_add']    = '<a href="customer_id=%d">%s</a> добавил(-а) новый адрес.';
-$_['text_customer_address_edit']   = '<a href="customer_id=%d">%s</a> обновил(-а) свой адрес.';
-$_['text_customer_address_delete'] = '<a href="customer_id=%d">%s</a> удалил(-а) один из своих адресов.';
-$_['text_customer_edit']           = '<a href="customer_id=%d">%s</a> обновил(-а) детали своего профиля.';
-$_['text_customer_forgotten']      = '<a href="customer_id=%d">%s</a> запросил(-а) новый пароль.';
-$_['text_customer_login']          = '<a href="customer_id=%d">%s</a> вошёл(-ла) в систему.';
-$_['text_customer_password']       = '<a href="customer_id=%d">%s</a> обновил(-а) свой пароль.';
-$_['text_customer_register']       = '<a href="customer_id=%d">%s</a> зарегистрировал(-а) новую учётную запись.';
-$_['text_customer_return_account'] = '<a href="customer_id=%d">%s</a> оформил(-а) возврат товара.';
-$_['text_customer_return_guest']   = '%s оформил(-а) возврат товара.';
-$_['text_customer_order_account']  = '<a href="customer_id=%d">%s</a> создал(-а) a <a href="order_id=%d">новый заказ</a>.';
-$_['text_customer_order_guest']    = '%s создал(-а) a <a href="order_id=%d">новый заказ</a>.';
-$_['text_affiliate_edit']          = '<a href="affiliate_id=%d">%s</a> обновил детали своей учётной записи.';
-$_['text_affiliate_forgotten']     = '<a href="affiliate_id=%d">%s</a> запросил(-а) новый пароль.';
-$_['text_affiliate_login']         = '<a href="affiliate_id=%d">%s</a> вошёл(-ла) в систему.';
-$_['text_affiliate_password']      = '<a href="affiliate_id=%d">%s</a> обновил(-а) свой пароль.';
-$_['text_affiliate_payment']       = '<a href="affiliate_id=%d">%s</a> обновил(-а) свои платёжные реквизиты.';
-$_['text_affiliate_register']      = '<a href="affiliate_id=%d">%s</a> зарегистрировал новую учётную запись.';
-
-// Column
-$_['column_order_id']              = 'Номер заказа';
-$_['column_customer']              = 'Покупатель';
-$_['column_status']                = 'Статус';
-$_['column_total']                 = 'Итого';
-$_['column_date_added']            = 'Дата добавления';
-$_['column_action']                = 'Действие';
+$_['text_day']                     = 'Сегодня';
+$_['text_week']                    = 'Неделя';
+$_['text_month']                   = 'Месяц';
+$_['text_year']                    = 'Год';
+$_['text_view']                    = 'Посмотреть еще...';
 
 // Error
-$_['error_install']                = 'Внимание: папка "Install" до сих пор существует и должна быть удалена в целях безопасности!';
+$_['error_install']                = 'Внимание! Инсталляционная директория все еще не удалена. Удалите ее незамедлительно, в противном случае вы рискуете полностью потерять свои данные!';
--- admin/language/russian/common/footer.php
+++ admin/language/russian/common/footer.php
@@ -1,3 +1,4 @@
 <?php
 // Text
-$_['text_footer'] = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.<br />Версия %s';
+$_['text_footer'] = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' Все права защищены.';
+$_['text_version'] 	= 'Версия %s';
