cec4ee70a096eedbfc3cdefd4877e14ff173e4b7
Ruslan Brest <rb@labtodo.com>
2014-10-07 15:15:54 +0300

    [catalog] sync/translate `common`, `error`, `information` folders

--- catalog/language/russian/common/header.php
+++ catalog/language/russian/common/header.php
@@ -1,20 +1,16 @@
 <?php
 // Text
-$_['text_home']     = 'Главная';
-$_['text_wishlist'] = 'Закладки (%s)';
-$_['text_shopping_cart']  = 'Корзина покупок';
-$_['text_category']      = 'Categories';
-$_['text_account']  = 'Постоянный покупатель';
-$_['text_register']      = 'Register';
-$_['text_login']         = 'Login';
-$_['text_order']         = 'Order History';
-$_['text_transaction']   = 'Transactions';
-$_['text_download']      = 'Downloads';
-$_['text_logout']        = 'Logout';
-$_['text_checkout'] = 'Оформление заказа';
-$_['text_search']   = 'Поиск';
-$_['text_all']           = 'See All';
-
-// REMOVED
-$_['text_welcome']  = '<a href="%s">Войти</a> или <a href="%s">зарегистрироваться</a>';
-$_['text_logged']   = 'Вы вошли как <a href="%s">%s</a> <b>(</b> <a href="%s">Выйти</a> <b>)</b>';
+$_['text_home']          = 'Главная';
+$_['text_wishlist']      = 'Закладки (%s)';
+$_['text_shopping_cart'] = 'Корзина покупок';
+$_['text_category']      = 'Категории';
+$_['text_account']       = 'Постоянный покупатель';
+$_['text_register']      = 'Регистрация';
+$_['text_login']         = 'Вход';
+$_['text_order']         = 'История заказов';
+$_['text_transaction']   = 'Финансовые операции';
+$_['text_download']      = 'Скачиваемые файлы';
+$_['text_logout']        = 'Выход';
+$_['text_checkout']      = 'Оформление заказа';
+$_['text_search']        = 'Поиск';
+$_['text_all']           = 'Полностью';
