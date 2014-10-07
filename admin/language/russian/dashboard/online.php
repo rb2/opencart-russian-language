e189cc5821e5ea53910b819fda0e2a7aa8fa9704
Sergey Ogarkov <sogarkov@gmail.com>
2014-10-07 17:03:41 +0300

    [admin] new folder admin/dashboard is translated

---
+++ admin/language/russian/dashboard/online.php
@@ -0,0 +1,6 @@
+<?php
+// Heading
+$_['heading_title'] = 'Посетители Online';
+
+// Text
+$_['text_view']     = 'Больше...';
---
+++ admin/language/russian/dashboard/recent.php
@@ -0,0 +1,11 @@
+<?php
+// Heading
+$_['heading_title']     = 'Недавние заказы';
+
+// Column
+$_['column_order_id']   = 'Код заказа';
+$_['column_customer']   = 'Покупатель';
+$_['column_status']     = 'Статус';
+$_['column_total']      = 'Всего';
+$_['column_date_added'] = 'Дата добавления';
+$_['column_action']     = 'Действие';
