0155442ca0589555eed69b80d8ff056138c23ac4
Ruslan Brest <rb@labtodo.com>
2014-10-07 19:40:54 +0300

    [admin] revert back some changes, fix menu wording

---
+++ admin/language/russian/common/stats.php
@@ -0,0 +1,4 @@
+<?php
+$_['text_complete_status']   = 'Завершённые заказы';
+$_['text_processing_status'] = 'Заказы в обработке';
+$_['text_other_status']      = 'Другие состояния';
--- admin/language/russian/common/status.php
+++
@@ -1,4 +0,0 @@
-<?php
-$_['text_complete_status']   = 'Заказы исполнены'; 
-$_['text_processing_status'] = 'Заказы исполняются'; 
-$_['text_other_status']      = 'Другие состояния'; 
