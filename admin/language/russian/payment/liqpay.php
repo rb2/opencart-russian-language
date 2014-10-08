92ddea274f49b27e74ee306323c1d17dad3aab84
Sergey Ogarkov <sogarkov@gmail.com>
2014-10-08 21:03:02 +0300

    [admin] admin/payment is synchronized. Some payment systems that are popular in Russian segment have been translated

--- admin/language/russian/payment/liqpay.php
+++ admin/language/russian/payment/liqpay.php
@@ -1,20 +1,19 @@
 <?php
-// Link
-$_['text_liqpay']        = '<img src="view/image/payment/liqpay.png" alt="LIQPAY" title="LIQPAY" style="border: 1px solid #EEEEEE;" />';
-
 // Heading
 $_['heading_title']      = 'LIQPAY';
 
 // Text
 $_['text_payment']       = 'Оплата';
 // $_['text_success']       = 'Настройки модуля обновлены!';
+$_['text_edit']          = 'Изменить настройки модуля "LIQPAY"';
 $_['text_pay']           = 'LIQPAY';
 $_['text_card']          = 'Credit Card';
+$_['text_liqpay']        = '<img src="view/image/payment/liqpay.png" alt="LIQPAY" title="LIQPAY" style="border: 1px solid #EEEEEE;" />';
 
 // Entry
 $_['entry_merchant']     = 'Merchant ID';
-$_['entry_signature']    = 'Signature';
-$_['entry_type']         = 'Type';
+$_['entry_signature']    = 'Сигнатура';
+$_['entry_type']         = 'Тип';
 $_['entry_total']        = 'Нижняя граница';
 $_['entry_order_status'] = 'Статус заказа после оплаты';
 $_['entry_geo_zone']     = 'Географическая зона';
--- admin/language/russian/payment/pp_standard.php
+++ admin/language/russian/payment/pp_standard.php
@@ -5,22 +5,17 @@ $_['heading_title']					 = 'PayPal Payments Standard';
 // Text
 $_['text_payment']					 = 'Оплата';
 // $_['text_success']                   = 'Настройки модуля обновлены!';
+$_['text_edit']                     = 'Изменить настройки модуля "PayPal Payments Standard"';
 $_['text_pp_standard']				 = '<a target="_BLANK" href="https://www.paypal.com/uk/mrb/pal=V4T754QB63XXL"><img src="view/image/payment/paypal.png" alt="PayPal Website Payment Pro" title="PayPal Website Payment Pro iFrame" style="border: 1px solid #EEEEEE;" /></a>';
 $_['text_authorization']			 = 'Авторизация';
 $_['text_sale']						 = 'Продажа';
 
-$_['tab_general']					 = 'Общее';
-$_['tab_status']					 = 'Статус заказа';
-
 // Entry
 $_['entry_email']					 = 'E-Mail';
 $_['entry_test']					 = 'Тестовый режим';
-$_['entry_test_help']    			 = 'Какой серверный шлюз (реальный или тестовый/sandbox) использовать для обработки транзакций?';
 $_['entry_transaction']				 = 'Способ проведения транзакций';
 $_['entry_debug']					 = 'Режим отладки';
-$_['entry_debug_help']				 = 'Сохраняет дополнительную информацию в системный лог.';
 $_['entry_total']                    = 'Нижняя граница';
-$_['entry_total_help']               = 'Минимальная сумма заказа. Ниже этой суммы метод будет недоступен.';
 $_['entry_canceled_reversal_status'] = 'Canceled Reversal Status';
 $_['entry_completed_status']         = 'Completed Status (завершённый)';
 $_['entry_denied_status']			 = 'Denied Status (отказ)';
@@ -34,6 +29,15 @@ $_['entry_voided_status']		     = 'Voided Status (аннулированный)'
 $_['entry_geo_zone']                 = 'Географическая зона';
 $_['entry_status']                   = 'Статус';
 $_['entry_sort_order']               = 'Порядок сортировки';
+
+// Tab
+$_['tab_general']					 = 'Общее';
+$_['tab_status']					 = 'Статус заказа';
+
+// Help
+$_['help_test']						= 'Какой серверный шлюз (реальный или тестовый/sandbox) использовать для обработки транзакций?';
+$_['help_debug']					= 'Сохраняет дополнительную информацию в системный лог.';
+$_['help_total']					= 'Минимальная сумма заказа. Ниже этой суммы метод будет недоступен.';
 
 // Error
 // $_['error_permission']               = 'У Вас нет прав для управления этим модулем!';
