d6102cec5b90c58f15a33aa84e62df904ec17311
Ruslan Brest <rb@labtodo.com>
2014-10-09 13:01:28 +0300

    [admin] fix translation of module/pp_login

--- admin/language/russian/module/pp_login.php
+++ admin/language/russian/module/pp_login.php
@@ -1,15 +1,15 @@
 <?php
 // Heading
-$_['heading_title']        = 'Вход в PayPal';
+$_['heading_title']        = 'Вход через PayPal';
 
-//Text
+// Text
 $_['text_module']          = 'Modules';
-//$_['text_success']         = 'Success: You have modified Log In with PayPal module!';
-$_['text_edit']            = 'Изменить настройки модуля "Вход в PayPal"';
+// $_['text_success']         = 'Success: You have modified Log In with PayPal module!';
+$_['text_edit']            = 'Изменить настройки модуля "Вход через PayPal"';
 $_['text_button_grey']     = 'Серый';
 $_['text_button_blue']     = 'Синий (Рекомендованный)';
 
-//Entry
+// Entry
 $_['entry_client_id']      = 'ID клиента';
 $_['entry_secret']         = 'Секретный код';
 $_['entry_sandbox']        = 'Режим песочницы';
@@ -21,7 +21,7 @@ $_['entry_locale']         = 'Локаль';
 $_['entry_return_url']     = 'URL возврата на сайт';
 $_['entry_status']         = 'Статус';
 
-//Help
+// Help
 $_['help_sandbox']         = 'Использовать тестовый режим, иммитирующий транзакции без реального списания денег со счета клиента (песочницу)?';
 $_['help_customer_group']  = 'Для новых покупателей, в какой группе покупателей они будут созданы?';
 $_['help_debug_logging']   = 'Этот режим позволяет записывать данные в ваш error log для облегчения отладки.';
@@ -29,7 +29,7 @@ $_['help_seamless']        = 'Обеспечивает авто-логин, ко
 $_['help_locale']          = 'Позволяет установить язык PayPal таким же, как и в магазине';
 $_['help_return_url']      = 'PayPal  использует этот URL для возврата в магазин.';
 
-//Error
-$_['error_permission']     = 'Warning: You do not have permission to modify Log In with PayPal module!';
-$_['error_client_id']      = 'Необходимо задать ID Клиента!';
-$_['error_secret']         = 'Необходимо задать секретный код!';
+// Error
+// $_['error_permission']     = 'Warning: You do not have permission to modify Log In with PayPal module!';
+$_['error_client_id']      = 'Укажите ID Клиента!';
+$_['error_secret']         = 'Укажите секретный код!';
